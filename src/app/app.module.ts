c150a51b86fa58d7483936a9e09d64480f79c4ca
FrankieLv <32904224+FrankieLv@users.noreply.github.com>
2023-02-26 18:42:45 +0800

    RedyForExercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { UserListComponent } from './angular-service-provider/user-list/user-list.component';
-import { UserService } from './angular-service-provider/service/user.service';
 import { FormsModule } from '@angular/forms';
-import { ProductFormComponent } from './angular-component-communication/product-form/product-form.component';
-import { ProductComponent } from './angular-component-communication/product/product.component';
-import { ProductTableComponent } from './angular-component-communication/product-table/product-table.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    UserListComponent,
-    ProductComponent,
-    ProductFormComponent,
-    ProductTableComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
-  providers: [UserService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
